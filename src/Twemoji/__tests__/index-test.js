002a3bd42f23300afc0c14b557ce081bdad20202
Zx <manyoushen@gmail.com>
2017-05-02 10:14:02 -0700

    Move react to peer dependency & upgrade to react@15.5

--- src/Twemoji/__tests__/index-test.js
+++ src/Twemoji/__tests__/index-test.js
@@ -2,7 +2,7 @@
 import { assert } from 'chai';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 
 import Twemoji from '..';
 
