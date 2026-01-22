757be25e09f72fdf3337cd5ab4613ebf1edb8de2
Zx <manyoushen@gmail.com>
2026-01-21 21:15:36 -0800

    nit

--- example/main.js
+++ example/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import from /client
+import { createRoot } from 'react-dom/client';
 import App from './App.js';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // Create a root.
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
